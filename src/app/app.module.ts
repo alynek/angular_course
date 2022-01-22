185778c11a008bf7e62bedd890a953e8fcbdf288
Alyne Gois <alyne0x67@gmail.com>
2022-01-21 23:20:00 -0300

    Add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './navigation/menu/menu.component';
 import { HomeComponent } from './navigation/home/home.component';
 import { FooterComponent } from './navigation/footer/footer.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
+import { rootRouterConfig } from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    SobreComponent,
+    ContatoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [RouterModule.forRoot(rootRouterConfig, {useHash: false})]
   ],
   providers: [],
   bootstrap: [AppComponent]
